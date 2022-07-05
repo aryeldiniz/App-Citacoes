30d9622b92bc24801e687129d5f681a5e35cdf5f
Aryel Diniz <aryel.agro@gmail.com>
2022-07-05 00:37:47 -0300

    Update main.dart
    Atualização do código fonte, primeiros passos na criação do aplicativo que irá gerar citações.

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -4,31 +4,46 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      //debugShowCheckedModeBanner: false,
-      title: "Frases Diárias",
-      home: Container(
-        //color: Color.fromARGB(255, 245, 245, 248),
-        padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-        margin: EdgeInsets.fromLTRB(10, 40, 10, 20),
-        decoration:
-            BoxDecoration(border: Border.all(width: 3, color: Colors.white54)),
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 60, 10, 60),
-              child: Text(
-                "A educação é a arma mais poderosa que você pode usar para mudar o mundo.",
-                textAlign: TextAlign.justify,
-                style: TextStyle(
-                  fontSize: 18,
-                  decoration: TextDecoration.none,
-                  decorationColor: Colors.white70,
-                  fontStyle: FontStyle.italic,
-                  color: Colors.white,
+      debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar:AppBar(
+            title: Text("Citações Nota 1000"),
+            backgroundColor: Color.fromARGB(255, 31, 105, 165),            
+          ),
+          body: Padding(
+            padding: EdgeInsets.all(20),
+            child:              
+              Text(
+                  "A educação é a arma mais poderosa que você pode usar para mudar o mundo.",
+                  style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontSize: 35,
+                    decoration: TextDecoration.none,
+                    decorationColor: Color.fromARGB(255, 17, 17, 18),
+                    fontWeight: FontWeight.bold
+                  ),
                 ),
+              ),        
+          bottomNavigationBar: BottomAppBar(
+            color: Color.fromARGB(255, 31, 105, 165),
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                children: [
+                  Text(
+                    "BOTÕES DE NAVEGAÇÃO",
+                  style: TextStyle(
+                    fontStyle: FontStyle.normal,
+                    fontSize: 20,
+                    decoration: TextDecoration.none,
+                    decorationColor: Color.fromARGB(255, 17, 17, 18),
+                    fontWeight: FontWeight.bold
+                    )
+                  ),
+                ],
               ),
-            )
-          ],
-        ),
-      )));
+              ),
+          ),
+        )
+        ));
 }
